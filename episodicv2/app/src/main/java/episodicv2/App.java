34813d5b5954a2b2e2c8b1308745ab4c2b6ff15d
Karen Baliero <karenbaliero.lima@gmail.com>
2023-04-27 08:57:20 -0300

    Add codelet groups

--- episodicv2/app/src/main/java/episodicv2/App.java
+++ episodicv2/app/src/main/java/episodicv2/App.java
@@ -66,10 +66,8 @@ public final class App {
         MemoryObject retrievedObjectRelationsTopDownMO;
         MemoryObject vLPFCtoVLPFCProcess1MO;
  
-        
-        
         m.createMemoryGroup("VENTRAL_MOs");
-        m.createMemoryGroup("DORSAL_MOS");
+        m.createMemoryGroup("DORSAL_MOs");
         m.createMemoryGroup("VISION_MOs");
         m.createMemoryGroup("CA3_MOs");
         m.createMemoryGroup("CA1_MOs");
@@ -77,26 +75,24 @@ public final class App {
         m.createMemoryGroup("SB_MOs");
         m.createMemoryGroup("PFC_MOs");
         m.createMemoryGroup("ENC_MOs");
-        
-        
-//        m.registerMemory(m1,"VENTRAL_MOs"); //ITC
-//        m.registerMemory(m1,"DORSAL_MOS"); //PHC
-//        
-//        m.registerMemory(m1,"CA3_MOs");
-//        m.registerMemory(m1,"CA1_MOs");
-//        m.registerMemory(m1,"DG_MOs");
-//        m.registerMemory(m1,"SB_MOs");
-//        m.registerMemory(m1,"PFC_MOs");
-//        m.registerMemory(m1,"ENC_MOs");
+        m.createCodeletGroup("VENTRAL_Cs");
+        m.createCodeletGroup("DORSAL_Cs");
+        m.createCodeletGroup("VISION_Cs");
+        m.createCodeletGroup("CA3_Cs");
+        m.createCodeletGroup("CA1_Cs");
+        m.createCodeletGroup("DG_Cs");
+        m.createCodeletGroup("SB_Cs");
+        m.createCodeletGroup("PFC_Cs");
+        m.createCodeletGroup("ENC_Cs");
 
         imagePerceptedMO=m.createMemoryObject("IMAGE_PERCEPTED");
         m.registerMemory(imagePerceptedMO,"VISION_MOs");
         
         boxCenterPointsMO=m.createMemoryObject("BOX_CENTER_POINTS");
-        m.registerMemory(boxCenterPointsMO,"DORSAL_MOS"); //PHC
+        m.registerMemory(boxCenterPointsMO,"DORSAL_MOs"); //PHC
         
         unintegratedScenePatternMO=m.createMemoryObject("UNINTEGRATED_SCENE_PATTERN");
-        m.registerMemory(unintegratedScenePatternMO,"DORSAL_MOS"); //PHC
+        m.registerMemory(unintegratedScenePatternMO,"DORSAL_MOs"); //PHC
         
         recognizedObjectsMO=m.createMemoryObject("RECOGNIZED_OBJECTS");
         m.registerMemory(recognizedObjectsMO,"VENTRAL_MOs"); //ITC
@@ -220,73 +216,67 @@ public final class App {
         
         vLPFCtoVLPFCProcess1MO=m.createMemoryObject("VLPFC_TO_VLPFC_PROCESS_1"); 
         m.registerMemory(vLPFCtoVLPFCProcess1MO,"PFC_MOs");
- 
-        
-        
-        // Create and populate MindViewer
-//        MindView mv = new MindView("MindView");
-//        mv.addMO(imagePerceptedaaMO);
         
         //Create Sensor Codelets
         Codelet vision=new Vision();
         vision.setName("VISION");
         vision.addOutput(imagePerceptedMO);
-        m.insertCodelet(vision);
+        m.insertCodelet(vision, "VISION_Cs");
         
         Codelet taskFrame=new TaskFrame();
         taskFrame.setName("TASK_NAME");
         taskFrame.addOutput(requestObjectRelationMO);
         taskFrame.addOutput(requestObjectMO);
-        m.insertCodelet(taskFrame);
+        m.insertCodelet(taskFrame, "VISION_Cs");
         
         Codelet pC=new PC();
         pC.setName("PC");
         pC.addInput(imagePerceptedMO);
         pC.addOutput(boxCenterPointsMO);
-        m.insertCodelet(pC);
+        m.insertCodelet(pC, "DORSAL_Cs");
         
         Codelet pCBridge=new PCBridge();
         pCBridge.setName("PC_BRIDGE");
         pCBridge.addInput(boxCenterPointsMO);
         pCBridge.addOutput(boxCenterPointsMO);
-        m.insertCodelet(pCBridge);
+        m.insertCodelet(pCBridge, "DORSAL_Cs");
         
         Codelet iTCObject=new ITCObject();
         iTCObject.setName("ITC_OBJECT");
         iTCObject.addInput(imagePerceptedMO);
         iTCObject.addOutput(recognizedObjectsMO);
-        m.insertCodelet(iTCObject);
+        m.insertCodelet(iTCObject, "VENTRAL_Cs");
         
         Codelet iTCFeatures=new ITCFeatures();
         iTCFeatures.setName("ITC_FEATURES");
         iTCFeatures.addInput(imagePerceptedMO);
         iTCFeatures.addOutput(iTCFeaturesMO);
-        m.insertCodelet(iTCFeatures);
+        m.insertCodelet(iTCFeatures, "VENTRAL_Cs");
         
         Codelet iTCBridge=new ITCBridge();
         iTCBridge.setName("ITC_BRIDGE");
         iTCBridge.addInput(recognizedObjectsMO);
         iTCBridge.addOutput(requestObjectMO);
         iTCBridge.addOutput(recognizedObjectsMO);
-        m.insertCodelet(iTCBridge);
+        m.insertCodelet(iTCBridge, "VENTRAL_Cs");
         
         Codelet iTCProcess1=new ITCProcess1();
         iTCProcess1.setName("ITC_PROCESS_1");
         iTCProcess1.addInput(requestObjectMO);
         iTCProcess1.addOutput(retrievedObjectTopDownMO);
-        m.insertCodelet(iTCProcess1);
+        m.insertCodelet(iTCProcess1, "VENTRAL_Cs");
         
         Codelet pHCBridge=new PHCBridge();
         pHCBridge.setName("PHC_BRIDGE");
         pHCBridge.addInput(boxCenterPointsMO);
         pHCBridge.addOutput(boxCenterPointsMO);
-        m.insertCodelet(pHCBridge);
+        m.insertCodelet(pHCBridge, "DORSAL_Cs");
         
         Codelet pHCProcess1=new PHCProcess1();
         pHCProcess1.setName("PHC_PROCESS_1");
         pHCProcess1.addInput(boxCenterPointsMO);
         pHCProcess1.addOutput(unintegratedScenePatternMO);
-        m.insertCodelet(pHCProcess1);
+        m.insertCodelet(pHCProcess1, "DORSAL_Cs");
         
         Codelet pRCBridge=new PRCBridge();
         pRCBridge.setName("PRC_BRIDGE");
@@ -294,34 +284,34 @@ public final class App {
         pRCBridge.addInput(recognizedObjectsMO);
         pRCBridge.addOutput(recognizedObjectsMO);
         pRCBridge.addOutput(requestObjectRelationMO);
-        m.insertCodelet(pRCBridge);
+        m.insertCodelet(pRCBridge, "VENTRAL_Cs");
         
         Codelet pRCProcess1=new PRCProcess1();
         pRCProcess1.setName("PRC_PROCESS_1");
         pRCProcess1.addInput(recognizedObjectsMO);
         pRCProcess1.addInput(requestObjectRelationMO);
         pRCProcess1.addOutput(recognizedObjectsMO);
-        m.insertCodelet(pRCProcess1);
+        m.insertCodelet(pRCProcess1, "VENTRAL_Cs");
         
         Codelet pRCProcess2=new PRCProcess2();
         pRCProcess2.setName("PRC_PROCESS_2");
         pRCProcess2.addInput(recognizedObjectsMO);
         pRCProcess2.addOutput(objectRelationMO);
-        m.insertCodelet(pRCProcess2);
+        m.insertCodelet(pRCProcess2, "VENTRAL_Cs");
         
         Codelet pRCProcess3=new PRCProcess3();
         pRCProcess3.setName("PRC_PROCESS_3");
         pRCProcess3.addInput(recognizedObjectsMO);
         pRCProcess3.addOutput(retrieveObjectRelationsMO);
         pRCProcess3.addOutput(pRCProcess3toVLPFC);
-        m.insertCodelet(pRCProcess3);
+        m.insertCodelet(pRCProcess3, "VENTRAL_Cs");
         
         Codelet pRCStorageHandler=new PRCStorageHandler();
         pRCStorageHandler.setName("PRC_STORAGE_HANDLER");
         pRCStorageHandler.addInput(objectRelationMO);
         pRCStorageHandler.addInput(retrieveObjectRelationsMO);
         pRCStorageHandler.addOutput(midTermMemoryObjectRelationsMO);
-        m.insertCodelet(pRCStorageHandler);
+        m.insertCodelet(pRCStorageHandler, "VENTRAL_Cs");
         
         Codelet eNC=new ENC();
         eNC.setName("ENC");
@@ -331,7 +321,7 @@ public final class App {
         eNC.addOutput(eNCtoDGandCA3);
         eNC.addOutput(eNCtoMPFC);
         eNC.addOutput(requestSomethingMO);
-        m.insertCodelet(eNC);
+        m.insertCodelet(eNC, "ENC_Cs");
         
         Codelet cA3=new CA3();
         cA3.setName("CA3");
@@ -341,14 +331,14 @@ public final class App {
         cA3.addOutput(newEncodedSceneMO);
         cA3.addOutput(cA3toCA3Process);
         cA3.addOutput(requestSomethingMO);
-        m.insertCodelet(cA3);
+        m.insertCodelet(cA3, "CA3_Cs");
         
         Codelet cA3Process1=new CA3Process1();
         cA3Process1.setName("CA3_PROCESS_1");
         cA3Process1.addInput(newEncodedSceneMO);
         cA3Process1.addOutput(sceneMO);
         cA3Process1.addOutput(newEncodedSceneMO);
-        m.insertCodelet(cA3Process1);
+        m.insertCodelet(cA3Process1, "CA3_Cs");
         
         Codelet cA3Process2=new CA3Process2();
         cA3Process2.setName("CA3_PROCESS_2");
@@ -356,15 +346,15 @@ public final class App {
         cA3Process2.addInput(similarScenesBottomUpMO);
         cA3Process2.addOutput(retrieveSimilarScenesBottomUpMO);
         cA3Process2.addOutput(cA3Process2toCA1);
-        m.insertCodelet(cA3Process2);
+        m.insertCodelet(cA3Process2, "CA3_Cs");
         
         Codelet cA3Process3=new CA3Process3();
-        cA3Process2.setName("CA3_PROCESS_3");
+        cA3Process3.setName("CA3_PROCESS_3");
         cA3Process3.addInput(requestSomethingMO);
         cA3Process3.addInput(similarScenesTopDownMO);
         cA3Process3.addOutput(retrieveSimilarScenesTopDownMO);
         cA3Process3.addOutput(retrievedSceneTopDownMO);
-        m.insertCodelet(cA3Process3);
+        m.insertCodelet(cA3Process3, "CA3_Cs");
         
         Codelet cA3StorageHandler=new CA3StorageHandler();
         cA3StorageHandler.setName("CA3_STORAGE_HANDLER");
@@ -374,7 +364,7 @@ public final class App {
         cA3StorageHandler.addOutput(similarScenesBottomUpMO);
         cA3StorageHandler.addOutput(similarScenesTopDownMO);
         cA3StorageHandler.addOutput(sceneStoreMO);
-        m.insertCodelet(cA3StorageHandler);
+        m.insertCodelet(cA3StorageHandler, "CA3_Cs");
         
         Codelet cA1=new CA1();
         cA1.setName("CA1");
@@ -385,13 +375,13 @@ public final class App {
         cA1.addOutput(cA1toCA1Process2andSB);
         cA1.addOutput(retrievedSceneTopDownLTMMO);
         cA1.addOutput(currentSceneMO);
-        m.insertCodelet(cA1);
+        m.insertCodelet(cA1, "CA1_Cs");
         
         Codelet cA1Process1=new CA1Process1();
         cA1Process1.setName("CA1_PROCESS_1");
         cA1Process1.addInput(newEncodedSceneMO);
         cA1Process1.addOutput(sceneRelationMO);
-        m.insertCodelet(cA1Process1);
+        m.insertCodelet(cA1Process1, "CA1_Cs");
         
         Codelet cA1Process2=new CA1Process2();
         cA1Process2.setName("CA1_PROCESS_2");
@@ -401,14 +391,14 @@ public final class App {
         cA1Process2.addOutput(sceneIDMO);
         cA1Process2.addOutput(retrievedSceneRelationTopDownMO);
         cA1Process2.addOutput(sceneRelationsMO);
-        m.insertCodelet(cA1Process2);
+        m.insertCodelet(cA1Process2, "CA1_Cs");
         
         Codelet cA1Process3=new CA1Process3();
         cA1Process3.setName("CA1_PROCESS_3");
         cA1Process3.addInput(retrievedSceneTopDownLTMMO);
         cA1Process3.addOutput(retrievedSceneRelationTopDownLTMMO);
         cA1Process3.addOutput(retrievedSceneRelationTopDownMO);
-        m.insertCodelet(cA1Process3);
+        m.insertCodelet(cA1Process3, "CA1_Cs");
         
         Codelet cA1StorageHandler=new CA1StorageHandler();
         cA1StorageHandler.setName("CA1_STORAGE_HANDLER");
@@ -416,25 +406,25 @@ public final class App {
         cA1StorageHandler.addInput(sceneIDMO);
         cA1StorageHandler.addOutput(midTermMemorySceneRelationsMO);
         cA1StorageHandler.addOutput(sceneRelationsArrayMO);
-        m.insertCodelet(cA1StorageHandler);
+        m.insertCodelet(cA1StorageHandler, "CA1_Cs");
         
         Codelet dGBridge=new DGBridge();
         dGBridge.setName("DG_BRIDGE");
         dGBridge.addInput(eNCtoDGandCA3);
         dGBridge.addOutput(dGtoDGProcess);
-        m.insertCodelet(dGBridge);
+        m.insertCodelet(dGBridge, "DG_Cs");
         
         Codelet dGProcess=new DGProcess();
         dGProcess.setName("DG_PROCESS");
         dGProcess.addInput(dGtoDGProcess);
         dGProcess.addOutput(newEncodedSceneMO);
-        m.insertCodelet(dGProcess);
+        m.insertCodelet(dGProcess, "DG_Cs");
         
         Codelet mPFC=new MPFC();
         mPFC.setName("MPFC");
         mPFC.addInput(eNCtoMPFC);
         mPFC.addOutput(mPFCtoMPFCProcess1andDLPFCMO);
-        m.insertCodelet(mPFC);
+        m.insertCodelet(mPFC, "PFC_Cs");
         
         Codelet mPFCProcess1=new MPFCProcess1();
         mPFCProcess1.setName("MPFC_PROCESS_1");
@@ -443,7 +433,7 @@ public final class App {
         mPFCProcess1.addInput(dLPFCtoMPFCProcess1MO);
         mPFCProcess1.addOutput(mPFCProcess1toENCMO);
         mPFCProcess1.addOutput(mPFCProcess1toDLPFCMO);
-        m.insertCodelet(mPFCProcess1);
+        m.insertCodelet(mPFCProcess1, "PFC_Cs");
         
         Codelet dLPFC=new DLPFC();
         dLPFC.setName("DLPFC");
@@ -452,19 +442,19 @@ public final class App {
         dLPFC.addInput(retrievedObjectTopDownMO);
         dLPFC.addInput(retrievedObjectRelationsTopDownMO);
         dLPFC.addOutput(taskSetMO);
-        m.insertCodelet(dLPFC);
+        m.insertCodelet(dLPFC, "PFC_Cs");
         
         Codelet dLPFCPlanning1=new DLPFCPlanning();
         dLPFCPlanning1.setName("DLPFC_PLANNING");
         dLPFCPlanning1.addOutput(taskSetMO);
-        m.insertCodelet(dLPFCPlanning1);
+        m.insertCodelet(dLPFCPlanning1, "PFC_Cs");
         
         Codelet dLPFCBaseController=new DLPFCBaseController();
         dLPFCBaseController.setName("DLPFC_PLANNING");
         dLPFCBaseController.addOutput(requestObjectMO);
         dLPFCBaseController.addOutput(requestObjectRelationMO);
         dLPFCBaseController.addOutput(vLPFCtoVLPFCProcess1MO);
-        m.insertCodelet(dLPFCBaseController);
+        m.insertCodelet(dLPFCBaseController, "PFC_Cs");
         
         Codelet vLPFC=new VLPFC();
         vLPFC.setName("VLPFC");
@@ -472,7 +462,7 @@ public final class App {
         vLPFC.addInput(retrievedObjectTopDownMO);
         vLPFC.addInput(iTCFeaturesMO);
         vLPFC.addOutput(vLPFCtoVLPFCProcess1MO);
-        m.insertCodelet(vLPFC);
+        m.insertCodelet(vLPFC, "PFC_Cs");
         
         Codelet vLPFCProcess1=new VLPFCProcess1();
         vLPFCProcess1.setName("VLPFC_PROCESS_1");
@@ -480,14 +470,14 @@ public final class App {
         vLPFCProcess1.addInput(requestObjectMO);
         vLPFCProcess1.addOutput(retrievedObjectTopDownMO);
         vLPFCProcess1.addOutput(requestObjectMO);
-        m.insertCodelet(vLPFCProcess1);
+        m.insertCodelet(vLPFCProcess1, "PFC_Cs");
         
         Codelet vLPFCProcess2=new VLPFCProcess2();
         vLPFCProcess2.setName("VLPFC_PROCESS_2");
         vLPFCProcess2.addInput(requestObjectRelationMO);
         vLPFCProcess2.addOutput(requestObjectRelationMO);
         vLPFCProcess2.addOutput(retrievedObjectRelationsTopDownMO);
-        m.insertCodelet(vLPFCProcess2);
+        m.insertCodelet(vLPFCProcess2, "PFC_Cs");
         
         Codelet sB=new SB();
         sB.setName("SB");
@@ -499,7 +489,7 @@ public final class App {
         sB.addInput(retrievedSceneRelationTopDownMO);
         sB.addInput(retrievedSceneRelationTopDownMO);
         sB.addOutput(sBtoENC);
-        m.insertCodelet(sB);
+        m.insertCodelet(sB, "SB_Cs");
        
         m.start();
         return(m);
